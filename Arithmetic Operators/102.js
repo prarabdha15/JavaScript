51b04be44150da8781773fc650f01e1eeeb18cb9
prarabdha15 <157135227+prarabdha15@users.noreply.github.com>
2024-03-12 16:42:42 +0530

    First Commit

---
+++ Arithmetic Operators/102.js
@@ -0,0 +1,43 @@
+/*
+
+Arithmetic expression is a combination of 
+operands (values, variables, etc..)
+operator (+ - / * % etc.)
+that can be evaluated to a value
+
+ex. y = x + 5;
+
+
+*/
+
+let students = 20;
+// students = students + 1;// Output 21
+// students = students - 1; // Output 20
+// students = students *2; // Output 40
+// students = students / 2; // Output 10
+
+// let extraStudents = students % 3; //Output 2
+
+students = students ** 3; //OUtput 400: exponentiation(to the power);
+
+
+// students += 1;
+// students -= 1;
+// students *=2;
+// students /=2;
+
+/*Operator Precedence
+
+1. Paranthesis()
+2. exponents
+3. Multiplication and division
+4. Addition and Subtraction
+
+*/
+
+let result = 1 + 2 * (3 + 4);
+
+
+
+console.log(result); 
+
