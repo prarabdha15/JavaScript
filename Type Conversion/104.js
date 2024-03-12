5e5b5725a90d8a052c7eac6c75cab0be5f525e99
prarabdha15 <157135227+prarabdha15@users.noreply.github.com>
2024-03-12 20:37:23 +0530

    First Commit

---
+++ Type Conversion/104.js
@@ -0,0 +1,26 @@
+// Type Conversion = change the datatype of a value to another 
+//            (strings, numbers, boolean)
+
+/*
+let age = window.prompt("How old are you?");
+// age = String(age);
+console.log(typeof age);
+age++;
+
+
+console.log(`You are ${age} years old!!!`);
+*/
+
+
+let x;
+let y;
+let z;
+
+
+x = Number("3.14");
+y = String(3.14);
+z = Boolean("");
+ 
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
