fea86af88b476b33fdbbb0cbe4d46eb3f3e437e2
prarabdha15 <157135227+prarabdha15@users.noreply.github.com>
2024-03-13 20:49:03 +0530

    First Commit

---
+++ If Statement/113.js
@@ -0,0 +1,19 @@
+// if statement = a basic form of decision making
+//                  if a condition is true, then do something
+//                  if not then dont do it.
+
+
+
+let age = 70;
+
+if(age >= 18 && age < 65){
+    console.log("You are an adult");
+}else if (age <0) {
+    console.log("You are not born yet");
+}else if (age >= 65){
+    console.log("You are a senior citizen");
+}
+
+else {
+    console.log("You are not an adult");
+}
