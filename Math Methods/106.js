f293e3972db2e918540218bf5ffcb704a0131b45
prarabdha15 <157135227+prarabdha15@users.noreply.github.com>
2024-03-13 09:32:01 +0530

    First Commit

---
+++ Math Methods/106.js
@@ -0,0 +1,21 @@
+let x = 3.14;
+let y = 5;
+let z = 9;
+let maximum;
+let minimum;
+
+
+// x = Math.round(x); // Output 3
+// x= Math.floor(x); //Output 3
+// x = Math.ceil(x); //Output 4
+// x = Math.pow(x, 3); //Output 63.52119900000001
+// x = Math.sqrt(x); // Output 1.772004514666935
+
+maximum = Math.max(x, y, z);
+console.log(maximum);
+minimum = Math.min(x, y, z);
+console.log(minimum);
+
+
+
+// console.log(x); // 
