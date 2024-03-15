bd2181e88f3b7f36af031611b04a8bd4b49acda5
prarabdha15 <157135227+prarabdha15@users.noreply.github.com>
2024-03-15 19:44:43 +0530

    First Commit

---
+++ Strict equality operator/116.js
@@ -0,0 +1,21 @@
+// = assignment operator
+
+// == comparison operator
+
+// === strict equality operator
+
+// let x = 3.14;
+
+// if(x == 3.14){
+//     console.log("That is pi"); //  That is pi
+// } else{
+// console.log("That is not pi");
+// }
+
+let x = "3.14";
+
+if(x === 3.14){
+    console.log("That is pi"); //  
+} else {
+    console.log("That is not pi"); //That is not pi
+} // Strict operator also compares the datatypes of the values being compared. 
