a3062152cc8132b0f215047fd234f391e30a8d96
prarabdha15 <157135227+prarabdha15@users.noreply.github.com>
2024-03-15 19:54:35 +0530

    First Commit

---
+++ Logical Operators/117.js
@@ -0,0 +1,22 @@
+// Gives us the ability to check more than 1 condition concurrently
+// && (Both conditions must be true)
+// || (Either condition must be true)
+
+
+// let temp = 67;
+ 
+// if(temp > 0 && temp < 30){
+//     console.log("Weather is good!!");
+// }else{
+//     console.log("Weather is bad!!");
+// }
+
+
+
+let temp = 15;
+ 
+if(temp <= 0 || temp >= 30){
+    console.log("Weather is good!!");
+}else{
+    console.log("Weather is bad!!");
+}
