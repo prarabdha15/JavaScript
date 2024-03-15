01373a24bac88231d53c468cffc97c4b94d0b100
prarabdha15 <157135227+prarabdha15@users.noreply.github.com>
2024-03-15 20:03:44 +0530

    First Commit

---
+++ Not logical Operator/118.js
@@ -0,0 +1,11 @@
+// NOT ! logical operator 
+// typically used to reverse a condition's boolean value
+// true-> false     false -> true
+
+
+let temp = 15;
+if(!(temp > 0)){
+    console.log("Its warm outside");
+}else{
+    console.log("Its cold outside");  //Its cold outside
+}
